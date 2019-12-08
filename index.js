455cd4e9a3aa67c27004476051b27e7d4407b5f4
Ginmo <c8juki00@students.oamk.fi>
2019-12-08 10:21:22 +0200

    jsonwebtoken testing

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.post('/users', (req, res) => {
             }
         }
     }).catch(() => {
-        res.sendStatus(500);
+        res.sendStatus(404);
     });
     //res.sendStatus(404);
 });
