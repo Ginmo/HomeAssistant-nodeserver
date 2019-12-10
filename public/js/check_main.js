a0350a5735c575bc33be8b62be15b09939d2be9a
Kimmo Juusola <c8juki00@students.oamk.fi>
2019-12-10 09:06:19 +0200

    minor fixes

--- public/js/check_main.js
+++ public/js/check_main.js
@@ -9,6 +9,7 @@ axios.get('/getstatus', {
 }).then(function (response) {
   if (response.status == 200) {
     console.log("go to main");
+    window.location.replace("/main");
   }else {
     console.log("go to login");
     window.location.replace("/login");
